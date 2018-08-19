07ac4b7e575bb04bc2238d05dd82c94de002bb75
Johnny <johnnyheath@hotmail.com>
2018-08-19 12:12:17 -0400

    Setting up server.js file

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const authRoutes = require("./routes/auth");
+const mongoose = require("mongoose");
+
+
+//Make sure that this is the correct thing to do here
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/river_rat");
+
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+// Use apiRoutes
+app.use("/api", apiRoutes);
+app.use("/auth", authRoutes);
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+// mongoose.Promise = require("bluebird");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/river_rat", { promiseLibrary: require("bluebird") })
+//   .then(() => console.log("connection successful"))
+//   .catch((err) => console.error(err));
